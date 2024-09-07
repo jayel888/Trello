212a1fb5bd0173aa3c10b2b71e66e7203c00a00d
jayel888 <15644@coderacademy.edu.au>
2024-09-07 10:31:37 +1000

    applied auth as admin decorator to card controller

--- utils.py
+++ utils.py
@@ -6,14 +6,14 @@ from init import db
 from models.user import User
 
 # Only admins can delete a card
-def authorise_as_admin():
-    # get the user's ID from get_jwt_identity
-    user_id = get_jwt_identity()
-    # fetch the user from the database
-    stmt = db.select(User).filter_by(id=user_id)
-    user = db.session.scalar(stmt)
-    # check whether the user is an admin or not
-    return user.is_admin
+# def authorise_as_admin():
+#     # get the user's ID from get_jwt_identity
+#     user_id = get_jwt_identity()
+#     # fetch the user from the database
+#     stmt = db.select(User).filter_by(id=user_id)
+#     user = db.session.scalar(stmt)
+#     # check whether the user is an admin or not
+#     return user.is_admin
 
 # Creating a decorator for authorise_as_admin
 
